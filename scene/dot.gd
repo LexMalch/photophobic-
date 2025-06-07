ae5e86154274f5c95b39570a0ed69d76065d1e1c
tracser <149647635+tracser@users.noreply.github.com>
2025-06-07 18:03:04 +0700

    m,bv

--- scene/dot.gd
+++ scene/dot.gd
@@ -1,7 +1,15 @@
 extends Node2D
-var id =0
+var id = 0
 
 
 func _on_area_2d_mouse_entered() -> void:
-	$AnimationPlayer.play("scale")
-	$label.text = str(id)
+	get_parent().firstPoint = self.position
+	get_parent().firstId = id
+	$Label.text = str(id)
+	if get_parent().mouse_pressed == true:
+		
+		get_parent().over.pop_back()
+		get_parent().over.append(self.position)
+		get_parent().over.append(self.position)
+		get_parent().curcode.append(id)
+		$AnimationPlayer.play("scale")
