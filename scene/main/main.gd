3b092bb8a532b27aa1fdb02b249f9dc86c03fbac
Мальчуков Алексей <106741316+LexMalch@users.noreply.github.com>
2025-06-08 14:58:03 +0700

    lkjb

--- scene/main/main.gd
+++ scene/main/main.gd
@@ -2,4 +2,7 @@ extends Node2D
 func _ready() -> void:
 	Global.change_shiza.connect(_local_change_shiza)
 func _local_change_shiza():
-	print("cock")	
+	if $PointLight2D.visible == true:
+		$PointLight2D.hide()
+	else:
+		$PointLight2D.show()
