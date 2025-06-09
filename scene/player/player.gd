43646cd05fcb59a9f59d2804ff912b0ec2060288
tracser <max-baiker7331@mail.ru>
2025-06-09 20:52:30 +0700

    123

--- scene/player/player.gd
+++ scene/player/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
-var speed = 200
+var speed = 100
 func _ready() -> void:
 	Global.change_shiza.connect(_local_change_shiza)
 func _physics_process(delta: float) -> void:
 	movement()
-	look_at(get_global_mouse_position())
+	$PointLight2D.look_at(get_global_mouse_position())
 func movement():
 	velocity = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized() * speed
 	move_and_slide()
---
+++ scene/player/point_light_2d.gd
@@ -0,0 +1,3 @@
+extends PointLight2D
+func _physics_process(delta):
+	look_at(get_global_mouse_position())
