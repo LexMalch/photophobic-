0c0fadbe4325a211820be91ca9d6f971cf4e91cc
Мальчуков Алексей <106741316+LexMalch@users.noreply.github.com>
2025-06-10 21:47:27 +0700

    dfghdfhg

--- scene/player/player.gd
+++ scene/player/player.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	Global.change_shiza.connect(_local_change_shiza)
 func _physics_process(delta: float) -> void:
 	movement()
-	$PointLight2D.look_at(get_global_mouse_position())
+	$Flashlight.look_at(get_global_mouse_position())
 func movement():
 	velocity = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized() * speed
 	move_and_slide()
