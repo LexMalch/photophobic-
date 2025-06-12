76137dcd308da107717965b84ea12cc1a473c3e0
tracser <max-baiker7331@mail.ru>
2025-06-12 22:30:06 +0700

    апо

--- scene/enemy/root_enemy.gd
+++ scene/enemy/root_enemy.gd
@@ -1,21 +1,34 @@
 extends CharacterBody2D
-var  seek_speed = 30
+var seek_speed = 30
 var chase_speed = 60
 var dir: Vector2 = Vector2.ZERO
 var damage_araa = null
+enum State{
+	CHASE,
+	SEEK,
+	ATTACK,
+	GETHIT
+	
+}
 
+var current_state = State.SEEK
 
-
-
-
-
+#func _process(delta: float) -> void:
 
 func  _physics_process(delta: float) -> void:
-	move()
-	if $Invin.time_left == 0:
-		if damage_araa != null:
-			take_damage
-			$Invin.start()
+	match current_state:
+		State.SEEK:
+			move()
+		State.CHASE:
+			pass
+		State.ATTACK:
+			pass
+		State.GETHIT:
+			if $Invin.time_left == 0:
+				if damage_araa != null:
+					take_damage
+					$Invin.start()
+			
 func move():
 	velocity = dir * seek_speed
 	move_and_slide()
@@ -23,20 +36,21 @@ func _on_seek_timer_timeout():
 	$seek_timer.start()
 	dir = (Vector2(randf_range(-50, 50), randf_range(-50, 50))).normalized()
 	print(dir)
-
-
 func _on_seek_area_body_entered(body):
-	$seek_timer.stop()
-	#dir = look_at(player.)
-	velocity = dir * chase_speed
+	if body.is_in_group("player"):
+		$seek_timer.stop()
+		var dir = (body.position - position).normalized()
+		velocity = dir * chase_speed
+		print(dir)
+func _on_chase_timer_timeout():
+	pass
 func _on_seek_area_body_exited(body):
+
 	$seek_timer.start()
 	velocity = dir * seek_speed
 
 func take_damage():
 	print("fafa")
-func _on_hitbox_body_entered(body):
-	damage_araa = body
-func _on_hitbox_body_exited(body):
-	if body == damage_araa:
-		damage_araa - null
+
+func _on_hitbox_area_entered(area):
+	pass # Replace with function body.
