ed4fb2b89e8a05356c4c889c50ae5e251f7353c0
tracser <max-baiker7331@mail.ru>
2025-06-12 17:18:54 +0700

    123

--- scene/enemy/root_enemy.gd
+++ scene/enemy/root_enemy.gd
@@ -2,9 +2,20 @@ extends CharacterBody2D
 var  seek_speed = 30
 var chase_speed = 60
 var dir: Vector2 = Vector2.ZERO
+var damage_araa = null
+
+
+
+
+
+
+
 func  _physics_process(delta: float) -> void:
 	move()
-	
+	if $Invin.time_left == 0:
+		if damage_araa != null:
+			take_damage
+			$Invin.start()
 func move():
 	velocity = dir * seek_speed
 	move_and_slide()
@@ -21,3 +32,11 @@ func _on_seek_area_body_entered(body):
 func _on_seek_area_body_exited(body):
 	$seek_timer.start()
 	velocity = dir * seek_speed
+
+func take_damage():
+	print("fafa")
+func _on_hitbox_body_entered(body):
+	damage_araa = body
+func _on_hitbox_body_exited(body):
+	if body == damage_araa:
+		damage_araa - null
