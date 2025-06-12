4ab1ce1b6eb9bc110a7273b07a38f663ad98b341
tracser <max-baiker7331@mail.ru>
2025-06-12 14:59:06 +0700

    123

--- scene/enemy/root_enemy.gd
+++ scene/enemy/root_enemy.gd
@@ -1,24 +1,23 @@
-extends RigidBody2D
-var seek_speed = 30
-var speed = 50
-var pos = position
-func _ready():
-	pass
-func _physics_process(delta):
-	pass
-
-func _on_seek_area_body_entered(body):
-	pass
-
-func _on_seek_area_body_exited(body):
-	pass # Replace with function body.
-func move():
-	apply_central_impulse(Vector2(randf_range(-50, 50), randf_range(-50, 50)))
-	
-
-
-func _on_seek_timer_timeout():
-	$seek_timer.start()
-	apply_central_impulse(Vector2(0,0))
+extends CharacterBody2D
+var  seek_speed = 30
+var chase_speed = 60
+var dir: Vector2 = Vector2.ZERO
+func  _physics_process(delta: float) -> void:
 	move()
 	
+func move():
+	velocity = dir * seek_speed
+	move_and_slide()
+func _on_seek_timer_timeout():
+	$seek_timer.start()
+	dir = (Vector2(randf_range(-50, 50), randf_range(-50, 50))).normalized()
+	print(dir)
+
+
+func _on_seek_area_body_entered(body):
+	$seek_timer.stop()
+	#dir = look_at(player.)
+	velocity = dir * chase_speed
+func _on_seek_area_body_exited(body):
+	$seek_timer.start()
+	velocity = dir * seek_speed
