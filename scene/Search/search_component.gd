b5c05b4255315e9a12d50336d66eb42488def0e4
Мальчуков Алексей <106741316+LexMalch@users.noreply.github.com>
2025-06-12 22:23:04 +0700

    cock

--- scene/Search/search_component.gd
+++ scene/Search/search_component.gd
@@ -4,3 +4,10 @@ func _ready() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	$CanvasLayer.visible = true
+	$AnimationPlayer.play("on_player")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	$AnimationPlayer.play_backwards("on_player")
+	await  get_tree().create_timer(0.5).timeout
+	$CanvasLayer.visible = false
